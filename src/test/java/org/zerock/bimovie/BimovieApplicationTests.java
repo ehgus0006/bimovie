b7c00bf4e49d30364269afb43ec616d314004a96
ehgus0006 <74715118+ehgus0006@users.noreply.github.com>
2021-10-05 16:05:23 +0900

    N:M 6강 완료

---
+++ src/test/java/org/zerock/bimovie/BimovieApplicationTests.java
@@ -0,0 +1,18 @@
+package org.zerock.bimovie;
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.zerock.bimovie.entity.Movie;
+import org.zerock.bimovie.entity.Poster;
+import org.zerock.bimovie.repository.MovieRepository;
+
+
+public class BimovieApplicationTests {
+
+
+
+
+
+}
